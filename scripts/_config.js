ce70da622e6626ba71fc6f172af458ca9849d816
wylfred <wylfredtapouamo@gmail.com>
2026-02-01 22:12:14 +0100

    traitement du popup

---
+++ scripts/_config.js
@@ -0,0 +1,5 @@
+
+const listeMots = ["Cachalot", "Pétunia", "Serviette"]
+const listePhrases = ["Pas de panique !", "La vie, l'univers et le reste", "Merci pour le poisson", "Le film était trop cool"]
+
+
